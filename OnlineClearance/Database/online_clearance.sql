85554ab3482033e2e10b9859fffc1f5490fd4cfe
ap32n <apegiwine1@gmail.com>
2022-07-31 23:12:22 -0800

    new database added

---
+++ OnlineClearance/Database/online_clearance.sql
@@ -0,0 +1,160 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 01, 2022 at 08:33 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `online_clearance`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `username` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`username`, `Password`) VALUES
+('admin', '1234');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+
+CREATE TABLE `department` (
+  `id` int(50) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `status` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `department`
+--
+
+INSERT INTO `department` (`id`, `Name`, `status`) VALUES
+(1, 'Campus Accountant', 'Rejected'),
+(2, 'Campus Librarian ', 'Accepted'),
+(3, 'Computer Laboratory', 'under process'),
+(4, 'Laboratory', 'under process'),
+(5, 'Senior Hall Tutor', 'under process'),
+(6, 'Campus Sports coach', 'under process'),
+(7, 'Dean in Charge', 'Rejected');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `request`
+--
+
+CREATE TABLE `request` (
+  `id` int(255) NOT NULL,
+  `department_id` int(255) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `request`
+--
+
+INSERT INTO `request` (`id`, `department_id`, `date`, `status`) VALUES
+(1, 1, '2022-07-25 23:14:47', 'Reject'),
+(2, 2, '2022-07-25 21:38:36', 'under process'),
+(3, 3, '2022-07-25 21:38:37', 'pending'),
+(4, 4, '2022-07-25 21:38:37', 'Under Procees'),
+(5, 5, '2022-07-25 21:38:37', 'pending'),
+(6, 6, '2022-07-25 21:38:37', 'pending'),
+(7, 7, '2022-07-25 21:38:37', 'pending');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `Index Number` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`username`, `password`, `fullname`, `Index Number`) VALUES
+('maxwell', '1234', 'Apegiwine Maxwell', 'fms/001/18'),
+('bransford', '1234', 'Boahene Bransford', 'fms/002/18'),
+('kobby', '1234', 'Edmond Addo', 'fms/003/18'),
+('kakape', '1234', 'Omari Stephen', 'fms/004/18'),
+('bismark', '1234', 'Owusu Bismark', 'fms/005/18'),
+('deborah', '1234', 'Deborah Attah', 'fms/006/18'),
+('Francis', '1234', 'Yacham Francis', 'fms/007/18');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `department`
+--
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `request`
+--
+ALTER TABLE `request`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`Index Number`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `department`
+--
+ALTER TABLE `department`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `request`
+--
+ALTER TABLE `request`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
