205197a695ada53b688766fb04e8ea234ced8752
flow1995 <stephenomari12@gmail.com>
2022-08-22 14:25:29 -0700

    worked on the admin response

--- OnlineClearance/admin/request.php
+++ OnlineClearance/admin/request.php
@@ -1,76 +1,59 @@
 <?php include "head.php";
 	  
-if (isset($_GET['Accept'])) 
+if (isset($_POST['status'])) 
 {
-$Accept=$_GET["Accept"];
-$studentid = $_GET['studentID'];
-$department = $_GET['department'];
-$status = $_GET['status'];
-$message = $_GET['message'];
-$sql="INSERT INTO clearacne(studentid,department,status) values("",'$studentid','$department',$message)  where id= '$Accept'";
-
+$studentid = $_POST['studentid'];
+$department = $_POST['department'];
+$status = $_POST['status'];
+$message = $_POST['message'];
+}
+$sql="INSERT INTO clearance SET studentid='$studentid', department='$department',status='$status',message='$message' where id= '$studentid'";
 if ($connect ->query($sql)===TRUE) {
-	echo "Accepted Successfully";}
+	echo '<script>alert("successfully Created")</script>'}
 }
 
-		
-if (isset($_GET["Reject"])) {
-	$Reject=$_GET["Reject"];
-	$sql="UPDATE department SET status='Rejected'where id='$Reject'";
-
-	if ($connect->query($sql)===TRUE) {
-		echo "Rejected Successfully";
-	}
-}
- ?>
-
-
-
-<div class="container " style="margin-top: 30px;">
+?>
+<div class='container' style="margin-top: 30px;">
 	<div class="row">
 		<div class="col-sm-12">
+			
 			<table class="table table-hover">
 				<tr>
 					<th><label for student_id><br>Student ID</label></th>
-					
-					<th><label for department><br>Department</label>
-					<select name="department" >
-						<option id="0"  >Campus Accountant</option>
-				 		<option id="1">Campus Librarian</optio>
-				 		<option id="2" >Computer Laboratory</option>
-				 		<option id="3" >Laboratory</option>
-				 		<option id="4" >Senior Hall Tutor</option>
-				 		<option id="5" >Campus Sports Coach</option>
-				  		<option id="6" >Dean in Charge</option>
-						</select></th>
-					<th><label for options><br>Options
-                      	<?php
-                  
-					
-					echo "<a href='request.php?Accept='.[id].' class='btn btn-success'>Accept</a>";
-					echo "<a href='request.php?Reject='.[id].' class='btn btn-danger'>Reject</a>";
-					
-			  ?>
-					</label></th>
+					<th><label for department><br>Department</label></th>
+					<th><label for options><br>Options</label></th>
 					<th><label for message><br>Message</label>
-					<input class="form control" type="text" name="message" required></th>
-
-					
 				</tr>
-		
-			  
+				<?php
+				$sql="SELECT * FROM student";
+				$studentList = $connect ->query($sql);
+				if ($studentList->num_rows > 0) {
+					while ( $record = $studentList->fetch_assoc()) {
 
+						echo "<tr><td><form action='request.php' method='POST'><p>".$record['Index Number']."</p><input type='hidden' name='studentid' placeholder='".$record['Index Number']."' value='".$record['Index Number']."'></td>
+							<td>
+							<select name='department' >
+							<option id='0'  >Campus Accountant</option>
+					 		<option id='1'>Campus Librarian</optio>
+					 		<option id='2' >Computer Laboratory</option>
+					 		<option id='3' >Laboratory</option>
+					 		<option id='4' >Senior Hall Tutor</option>
+					 		<option id='5' >Campus Sports Coach</option>
+					  		<option id='6' >Dean in Charge</option>
+							</select>
+							<td>
+							<button type='submit' name='status' value='accept' class='btn btn-success'>Accept</button>
+							<button type='submit' name='status' value='reject' class='btn btn-success'>Reject</button>
+							<td><input class='form control' type='text' name='message' required>"."</td></form>
+							</tr>";
+					}
+				}
+
+
+				?>
 			</table>
 		</div>
 	</div>
 </div>
-
-
-
-
-
-
-
-
 
 <?php include "foot.php"  ?>
